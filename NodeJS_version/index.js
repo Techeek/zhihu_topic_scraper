7e819aeaea0ff8de9e55bcac7d17e26c6fcf795c
Techeek <qc_957a692741d9444b9f25c53a3da69e7a@git.code.tencent.com>
2018-11-20 20:10:34 +0800

    更新链接

--- NodeJS_version/index.js
+++ NodeJS_version/index.js
@@ -3,14 +3,13 @@ var fs = require("fs");
 var sleep = require('system-sleep');
 var csvToJson = require('convert-csv-to-json');
 var csv_json = csvToJson.getJsonFromCsv("input.csv");
-fs.appendFile('output.csv',"标签" + '\t' + "问题数" + '\t' + "关注数" + '\t' + "精选数" + '\t' + "简介" + '\r\n' , function (err) {
+fs.appendFile('output.csv',"标签" + '\t' + "问题数" + '\t' + "关注数" + '\t' + "精选数" + '\t' + "简介" + '\t' + "url" + '\r\n' , function (err) {
   if (err) throw err;
 });
 for(i=0;i < csv_json.length;i++){
   sleep(500)
   url_num = csv_json[i].url_num
   url_list = 'https://www.zhihu.com/api/v4/topics/' + url_num
-  console.log(url_list)
   var options = { method: 'GET',
     url: url_list,
     headers: 
@@ -19,12 +18,14 @@ for(i=0;i < csv_json.length;i++){
   request(options, function (error, response, body) {
     data = unescape(body.replace(/\\u/g, '%u'))
     data_json_parse =  JSON.parse(data)
+    url_original = 'https://www.zhihu.com/topic/' + url_num + '/hot'
+    console.log("url：" + url_original)
     console.log("标签：" + data_json_parse.name)
     console.log("问题数：" + data_json_parse.questions_count)
     console.log("关注数：" + data_json_parse.followers_count)
     console.log("简介：" + data_json_parse.introduction)
     console.log("精选数：" + data_json_parse.best_answers_count)
-    var data_csv = data_json_parse.name + '\t' + data_json_parse.questions_count + '\t' + data_json_parse.followers_count + '\t' + data_json_parse.best_answers_count + '\t' + data_json_parse.introduction;
+    var data_csv = data_json_parse.name + '\t' + data_json_parse.questions_count + '\t' + data_json_parse.followers_count + '\t' + data_json_parse.best_answers_count + '\t' + data_json_parse.introduction + '\t' + url_original;
     fs.appendFile('output.csv',data_csv + '\r\n', function (err) {
       if (err) throw err;
    });
